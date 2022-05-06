e45f3aac75cf1872124c577798a1fc078071027c
BULANOV Andrey V <andrey.bulanov@raiffeisen.ru>
2022-05-06 14:51:17 +0300

    homework8

--- src/pro/sky/Main.java
+++ src/pro/sky/Main.java
@@ -4,5 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        // 1) print
+        String firstName = "Семён";
+        String middleName = "Игоревич";
+        String lastName = "Орлов";
+        String fullName = lastName +" "+firstName+" "+middleName;
+        System.out.println("ФИО сотрудника — "+fullName+".");
+
+        // 2) upper case
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName.toUpperCase());
+
+        // 3) ё -> e
+        fullName = fullName.replace('ё','е').replace('Ё','E');
+        System.out.println("Данные ФИО сотрудника — "+fullName);
     }
 }
